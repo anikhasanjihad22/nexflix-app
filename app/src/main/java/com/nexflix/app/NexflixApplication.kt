5babc7ca4ddb087df61d49333f584d7bad2d9624
Anik Hasan Jihad <anikhasanjihad71@gmail.com>
2026-08-14 11:47:21 +0000

    Catch crash even earlier

--- app/src/main/java/com/nexflix/app/NexflixApplication.kt
+++ app/src/main/java/com/nexflix/app/NexflixApplication.kt
@@ -1,28 +1,29 @@
 package com.nexflix.app
 
 import android.app.Application
+import android.content.Context
 import android.content.Intent
 
 class NexflixApplication : Application() {
 
-        override fun onCreate() {
-                    super.onCreate()
+            override fun attachBaseContext(base: Context?) {
+                        super.attachBaseContext(base)
 
-                            Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
-                                        try {
-                                                            val trace = throwable.stackTraceToString()
-                                                                            val intent = Intent(this, CrashActivity::class.java)
-                                                                                            intent.putExtra("error", trace)
-                                                                                                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                                                                                                                            startActivity(intent)
-                                        } catch (e: Exception) {
-                                        }
-                                                    android.os.Process.killProcess(android.os.Process.myPid())
-                                                                System.exit(10)
-                            }
-        }
+                                Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+                                            try {
+                                                                val trace = throwable.stackTraceToString()
+                                                                                val intent = Intent(this, CrashActivity::class.java)
+                                                                                                intent.putExtra("error", trace)
+                                                                                                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                                                                                                                startActivity(intent)
+                                            } catch (e: Exception) {
+                                            }
+                                                        android.os.Process.killProcess(android.os.Process.myPid())
+                                                                    System.exit(10)
+                                }
+            }
 }
-                                        }
-                                        }}
-        }
+                                            }
+                                            }}
+            }
 }
